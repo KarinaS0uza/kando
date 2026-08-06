9e8281c57c50727c0acb9b7f52a990bd043b64cc
NicolasSG <neco.beatles@gmail.com>
2026-08-06 19:28:18 -0300

    style: block browser dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <>
+    <MantineProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </>
+    </MantineProvider>
   );
 }
 
